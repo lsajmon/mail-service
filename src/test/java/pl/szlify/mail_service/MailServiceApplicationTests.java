04aec166237853c359b71bebebc1217dcf098e25
Sajmon <linowski.szymon@gmail.com>
2025-11-14 21:35:31 +0100

    Innit

---
+++ src/test/java/pl/szlify/mail_service/MailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.szlify.mail_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
